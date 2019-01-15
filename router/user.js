d0056061fbd2b157fa698d620c1c960093a574b7
MadaoDemo <1429544545@qq.com>
2019-01-15 21:00:24 +0800

    order页面的逻辑

--- router/user.js
+++ router/user.js
@@ -348,4 +348,24 @@ router.get('/delete_shop_car', (req, res) => {
     })
 })
 
+router.post('/getOrderInfo', (req, res) => {
+    var obj = req.body;
+    var sid = obj.sid;
+    var user = obj.user;
+    var sql = `SELECT shop.shop_name,shop.deliver_fee,shop.deliver_time,food.name,shop_car.number,shop_car.un_price 
+    FROM food JOIN shop_car ON food.food_id=shop_car.fid JOIN shop ON food.shop_id=shop.id JOIN user ON shop_car.uid=user.id 
+    WHERE user.phone=? AND shop.id=?
+    `;
+    pool.query(sql, [user,sid], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        if (result) {
+            res.send(result);
+        } else {
+            res.send({code:400, msg:'get order info fault!'})
+        }
+    })
+})
+
 module.exports = router;
