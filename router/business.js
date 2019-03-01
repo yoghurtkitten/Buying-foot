8c5c1517151811bb8f1aea6c235a028190a94475
MadaoDemo <1429544545@qq.com>
2019-03-01 21:00:46 +0800

    商家注册完成

--- router/business.js
+++ router/business.js
@@ -14,12 +14,38 @@ router.get('/Login', (req, res) => {
                 name: result[0].phone
             }
             req.session.business = business;
-            console.log(req.session.business.name)
             res.send({ code: 200, data: result });
         } else {
             res.send({ code: 400, data: '登录失败' })
         }
     })
 });
+router.post('/registe', (req, res) => {
+    // res.send(req.body);
+    var obj = req.body;
+    var sql = 'INSERT INTO business VALUES (null, ?,?,?,?,?,?)';
+    pool.query(sql, [obj.password, obj.phone, obj.shopman_name, obj.idCard, obj.business_img, obj.contact], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        if (result.affectedRows) {
+            sql = `INSERT INTO shop (business_id, shop_name, shop_type, province, city, county, address, license, shop_img, shop_phone, licenseName, isPass) 
+            VALUES ((SELECT max(id) from business), ?, ?, ?,?,?,?,?,?,?,?,0)`;
+            pool.query(sql, [obj.shop_name, obj.shop_type, obj.province, obj.city, obj.county, obj.address, obj.license, obj.shop_img, obj.shop_phone, obj.licenseName], (err, result) => {
+                if (err) {
+                    throw err;
+                }
+                if (result.affectedRows) {
+                    res.send({ code: 200, msg: '插入成功' });
+                } else {
+                    res.send({ code: 200, msg: '商家信息插入失败' })
+                }
+            })
+        } else {
+            res.send({ code: 200, msg: '商家信息插入失败' })
+        }
+    })
+
+})
 
 module.exports = router;
